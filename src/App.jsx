d384d5340c6bbe2d87f58b7b90e03561c697daaa
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-26 11:37:24 +0200

    App.jsx setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import './App.css';
+import { Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Rooms from "./pages/Rooms";
+import Contact from "./pages/Contact";
+import Users from "./pages/Users";
 
 function App() {
-  return (<div></div>);
+  return (
+    <Routes>
+      <Route path="/">
+        <Dashboard />
+      </Route>
+      <Route path="/Bookings">
+        <Bookings />
+        </Route>
+      <Route path="/Rooms">
+        <Rooms />
+        </Route>
+      <Route path="/Contact">
+        <Contact />
+        </Route>
+      <Route path="/Users">
+        <Users />
+        </Route>
+    </Routes>
+  );
 }
 
 export default App;
