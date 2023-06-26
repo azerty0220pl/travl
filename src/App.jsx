6928426e70f5dfbe379e025cf53abce1c57235f3
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-26 19:49:33 +0200

    Routes fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Rooms from "./pages/Rooms";
 import Contact from "./pages/Contact";
 import Users from "./pages/Users";
+import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/">
-        <Dashboard />
-      </Route>
-      <Route path="/Bookings">
-        <Bookings />
-        </Route>
-      <Route path="/Rooms">
-        <Rooms />
-        </Route>
-      <Route path="/Contact">
-        <Contact />
-        </Route>
-      <Route path="/Users">
-        <Users />
-        </Route>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/rooms" element={<Rooms />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/users" element={<Users />} />
     </Routes>
   );
 }
