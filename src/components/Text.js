00f1d6829de4dfca2efac75ea2162a9ca11f989b
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-27 13:50:03 +0200

    Minopr text upgrade

--- src/components/Text.js
+++ src/components/Text.js
@@ -4,10 +4,12 @@ const Text = styled.p`
     color: ${props => props.color || "black"};
     text-align: ${props => props.align || "start"};
     font-size: ${props => props.size || "1rem"};
+    line-height: ${props => props.size || "1rem"};
     font-weight: ${props => props.weight || "400"};
     text-decoration: none;
     width:  ${props => props.width || "100%"};
     word-wrap: break-word;
+    margin-top: ${props => props.margin || "0"};
 `;
 
 export default Text;
