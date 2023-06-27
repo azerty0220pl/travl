72bc308db4063aa35b30edb5d2c1f68b4c339a57
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-27 10:49:07 +0200

    Reservations Entries added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
 import Menu from "../components/Menu";
-import { Entry, Icon } from "../components/Box";
+import { Box, Entry, Icon } from "../components/Box";
 import Text from "../components/Text";
 import { LiaBedSolid, LiaCalendarCheck } from 'react-icons/lia';
 import { BiLogIn, BiLogOut } from 'react-icons/bi';
+import empty from "../assets/empty.png";
+import ReservationEntry from "../components/ReservationEntry";
 
 const Dashboard = () => {
     return (
@@ -46,6 +48,30 @@ const Dashboard = () => {
                     </div>
                 </Entry>
             </Entry>
+            <Box margin="0 1rem" >
+                <ReservationEntry
+                    room="Deluxe Duplex Room"
+                    number="001"
+                    photo={empty}
+                    name="Orlando Bloom"
+                    from="27/07/2023"
+                    to="29/07/2023" />
+                <ReservationEntry
+                    room="Minimal Monplex Room"
+                    number="505"
+                    photo={empty}
+                    name="Amber Heard"
+                    from="27/07/2023"
+                    to="29/07/2023" />
+                <ReservationEntry
+                    room="Minimal Quadruplex Room"
+                    number="303"
+                    photo={empty}
+                    name="John Lennon"
+                    from="27/07/2023"
+                    to="29/07/2023"
+                    last />
+            </Box>
         </div>
     );
 }
