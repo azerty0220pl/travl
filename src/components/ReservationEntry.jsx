4af36f940d3a582c1d5ed7dc35dbd9d020375920
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-27 10:48:54 +0200

    Last Margin upgrade

--- src/components/ReservationEntry.jsx
+++ src/components/ReservationEntry.jsx
@@ -1,18 +1,20 @@
 import { Entry, Box } from "./Box";
 import Text from "./Text";
 
-const ReservationEntry = ({ room, number, name, from, to }) => {
+const ReservationEntry = ({ room, number, name, from, to, photo, last }) => {
 
-    <Entry padding="0" margin="0 0 1rem 0" justify="space-between">
-        <Entry padding="0" margin="0">
-            <Box margin="0" padding="0" height="5rem" width="7rem" as="img" src={empty} />
-            <div>
-                <Text weight="600" size="1.25rem">{room} - {number}</Text>
-                <Text>{name}</Text>
-            </div>
+    return (
+        <Entry padding="0" margin={last ? "0" : "0 0 1rem 0"} justify="space-between">
+            <Entry padding="0" margin="0">
+                <Box margin="0" padding="0" height="5rem" width="7rem" as="img" src={photo} />
+                <div>
+                    <Text weight="600" size="1.25rem">{room} - {number}</Text>
+                    <Text>{name}</Text>
+                </div>
+            </Entry>
+            <Text width="fit-content">{from} - {to}</Text>
         </Entry>
-        <Text width="fit-content">{from} - {to}</Text>
-    </Entry>
+    );
 }
 
 export default ReservationEntry;
