1b54602299dcdde81d0607c3445705fc4977c625
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-27 15:11:50 +0200

    Message created

---
+++ src/components/Message.jsx
@@ -0,0 +1,17 @@
+import { Box, Entry } from "./Box";
+import Text from "./Text";
+
+const Message = ({name, mail, subject, message}) => {
+    return (
+        <Box width="100%" height="100%" border='1px solid #EBEBEB'>
+            <Text weight="600">{subject}</Text>
+            <Box height="3.75rem" width="100%" margin="1rem 0" padding="0">
+                <Text line="1.25rem">{message}</Text>
+            </Box>
+            <Text weight="600">{name}</Text>
+            <Text margin="0.5rem" size="0.75rem" color="#799283">{mail}</Text>
+        </Box>
+    );
+};
+
+export default Message;
