9e2f3a763b39c2270116052bacb523e61d7973ea
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-28 10:43:09 +0200

    Table upgrade

---
+++ src/components/Table.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Table = styled.div`
+    display: table;
+    width: 100%;
+`;
+
+export const Row = styled.div`
+    display: table-row;
+    width: 100%;
+`;
+
+export const Cell = styled.div`
+    display: table-cell;
+    padding: 1rem 0;
+`;
