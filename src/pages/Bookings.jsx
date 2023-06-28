4e0c026b4de21aa040fbeb4c0132f039e21238f5
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-28 12:07:31 +0200

    Bookings Table Upgrade

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -22,37 +22,83 @@ const Select = styled(Text)`
 const Page = () => {
     let data = [
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Refund"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "In Progress"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "In Progress"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Refund"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Refund"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Refund"],
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Refund"],
         ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"],
-        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "Booked"]
+        ["Orlando Bloom", "18/07/2023", "27/07/2023", "29/07/2023", "hello", "Deluxe Duplex Bed", "In Progress"]
     ];
+
+    let Status = (x) => {
+        switch (x) {
+            case "Booked":
+                return (
+                    <Box margin="0" padding="1rem" height="3rem" color="#E8FFEE">
+                        <Text align="center" color="#5AD07A">{x}</Text>
+                    </Box>
+                );
+            case "Refund":
+                return (
+                    <Box margin="0" padding="1rem" height="3rem" color="#FFEDEC">
+                        <Text align="center" color="#E23428">{x}</Text>
+                    </Box>
+                );
+            case "In Progress":
+                return (
+                    <Box margin="0" padding="1rem" height="3rem" color="#FFFEEC">
+                        <Text align="center" color="#dfe228">{x}</Text>
+                    </Box>
+                );
+        }
+    }
 
     data = data.map((el, i) => {
         return (
             <Row key={i}>
-                {
-                    el.map((el, i) => {
-                        return (
-                            <Cell>
-                                <Text>{el}</Text>
-                            </Cell>
-                        )
-                    })
-                }
+                <Cell>
+                    <Text weight="600">{el[0]}</Text>
+                </Cell>
+                <Cell>
+                    <Text>{el[1]}</Text>
+                </Cell>
+                <Cell>
+                    <Text>{el[2]}</Text>
+                </Cell>
+                <Cell>
+                    <Text>{el[3]}</Text>
+                </Cell>
+                <Cell>
+                    {
+                        el[4].length === 0 ?
+                            <Box margin="0 2rem" padding="1rem" height="3rem" border="2px solid #799283">
+                                <Text align="center" color="#799283">View Notes</Text>
+                            </Box>
+                            :
+                            <Box margin="0 2rem 0 0" padding="1rem" height="3rem" color="#EEF9F2">
+                                <Text align="center">View Notes</Text>
+                            </Box>
+                    }
+                </Cell>
+                <Cell>
+                    <Text>{el[5]}</Text>
+                </Cell>
+                <Cell>
+                    {
+                        Status(el[6])
+                    }
+                </Cell>
             </Row>
         );
     })
