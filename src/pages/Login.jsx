e74a7d2b999d86a5812e0fcf8efaf93a0ae14743
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-29 13:16:04 +0200

    loginLogic used

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,58 @@
 import { Box, Entry } from "../components/basic/Box";
 import Text from "../components/basic/Text";
 import { useNavigate } from "react-router-dom";
+import { login, logged } from "../components/basic/loginLogic";
+import { useState, useEffect } from "react";
 
 const Login = () => {
     const navigate = useNavigate();
+    let [err, setErr] = useState(false);
 
-    const handleSubmit = (event) => {
-        if(event.target.user.value === "admin" && event.target.password.value === "password") {
-            localStorage.setItem("session", "admin");
+    useEffect(() => {
+        if (logged()) {
             navigate("/");
         }
+    })
+
+    const handleSubmit = (event) => {
+        if (login(event.target.user.value, event.target.password.value)) {
+            navigate("/");
+            setErr(false);
+        }
+        else
+            setErr(true);
     }
 
-    return (
-        <Entry justify="center" margin="0" padding="0" color="transparent" height="100vh" width="100%">
-            <Box margin="auto" width="fit-content">
-                <Text weight="600">Login:</Text>
-                <Box margin="1rem 0 0 0" padding="1rem" border="1px solid #EBEBEB">
-                    <form onSubmit={handleSubmit}>
-                        <label id="user-label" htmlFor="user">
-                            <Text>Username:</Text>
-                            <Text as="input" margin="0.5rem" type="text" id="user" placeholder="admin" required />
-                        </label>
-                        <label id="password-label" htmlFor="password">
-                            <Text margin="1rem">Password:</Text>
-                            <Text as="input" margin="0.5rem" type="password" id="password" placeholder="password" required />
-                        </label>
-                        <Box as="button" color="#EEF9F2" margin="1rem auto 0 auto" padding="1rem">
-                            <Text align="center">Login</Text>
-                        </Box>
-                    </form>
+    if (localStorage)
+
+        return (
+            <Entry justify="center" margin="0" padding="0" color="transparent" height="100vh" width="100%">
+                <Box margin="auto" width="fit-content">
+                    <Text weight="600">Login:</Text>
+                    <Box margin="1rem 0 0 0" padding="1rem" border="1px solid #EBEBEB">
+                        <form onSubmit={handleSubmit}>
+                            <label id="user-label" htmlFor="user">
+                                <Text>Username:</Text>
+                                <Text as="input" margin="0.5rem" type="text" id="user" placeholder="admin" required />
+                            </label>
+                            <label id="password-label" htmlFor="password">
+                                <Text margin="1rem">Password:</Text>
+                                <Text as="input" margin="0.5rem" type="password" id="password" placeholder="password" required />
+                            </label>
+                            {
+                                err ?
+                                    <Text color="red" size="0.75rem" margin="0.5rem">Incorrect password</Text>
+                                    :
+                                    <></>
+                            }
+                            <Box as="button" color="#EEF9F2" margin="1rem auto 0 auto" padding="1rem">
+                                <Text align="center">Login</Text>
+                            </Box>
+                        </form>
+                    </Box>
                 </Box>
-            </Box>
-        </Entry>
-    );
+            </Entry>
+        );
 }
 
 export default Login;
