daa855713015c74511b1bc53376a796fba75ca17
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-30 10:19:18 +0200

    Margin added to select

--- src/components/basic/Select.js
+++ src/components/basic/Select.js
@@ -2,6 +2,7 @@ import Text from "./Text";
 import { styled } from "styled-components";
 
 const Select = styled(Text)`
+    margin: ${props => props.margin || "0"};
     border: 2px solid #135846;
     width: auto;
     border-radius: 0.75rem;
