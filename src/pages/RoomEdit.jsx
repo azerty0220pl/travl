a7f00bd7dfa855f706975ea389e63f19156f33d1
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-30 09:29:04 +0200

    RoomEdit form template from userEdit

--- src/pages/RoomEdit.jsx
+++ src/pages/RoomEdit.jsx
@@ -2,57 +2,60 @@ import Menu from "../components/advanced/Menu";
 import empty from "../assets/empty.png";
 import { Icon, Entry, Box, BoxAbsolute } from "../components/basic/Box";
 import Text from "../components/basic/Text";
+import Select from "../components/basic/Select";
 
 const Page = () => {
     return (
         <Entry margin="1rem" padding="2rem" justify="space-between">
-            <Box margin="0" padding="1rem" width="50%">
-                <Text weight="600">Full Name Here</Text>
-                <Text color="#799283" size="0.75rem" margin="0.5rem">#idididid</Text>
+            <Box as="form" margin="0" padding="1rem" border="1px solid #EBEBEB" width="50%">
+                <label id="user-label" htmlFor="user">
+                    <Text weight="600">Username:</Text>
+                    <Text as="input" type="text" id="user" placeholder="username" required />
+                </label>
                 <Entry margin="1rem 0 0 0" padding="0" radius="0" justify="space-between">
-                    <Box margin="0" padding="0" width="50%" radius="0">
-                        <Text size="0.875" color="#6E6E6E">Check In:</Text>
-                        <Text margin="0.5rem" weight="600">12/12/21</Text>
+                    <Box as="label" id="mail-label" htmlFor="mail" margin="0" padding="0" radius="0" height="100%" width="100%">
+                        <Text weight="600">Email:</Text>
+                        <Text as="input" type="mail" id="mail" placeholder="mail@mail.mail" required />
                     </Box>
-                    <Box margin="0" padding="0" width="50%" radius="0">
-                        <Text size="0.875" color="#6E6E6E">Check Out:</Text>
-                        <Text margin="0.5rem" weight="600">12/12/12</Text>
+                    <Box as="label" id="phone-label" htmlFor="phone" margin="0" padding="0" radius="0" height="100%" width="100%">
+                        <Text weight="600">Phone:</Text>
+                        <Text as="input" type="number" id="phone" placeholder="987654321" required />
                     </Box>
                 </Entry>
                 <Entry margin="1rem 0 0 0" padding="0" radius="0" justify="space-between">
-                    <Text weight="600" width="50%" margin="1rem">Minimal Duplex Deluxe - A098</Text>
-                    <Entry margin="0" padding="0" gap="0" radius="0" justify="space-between">
-                        <Text margin="1rem">$145</Text>
-                        <Text margin="1rem" size="0.75rem" color="#6E6E6E">/night</Text>
-                    </Entry>
-                </Entry>
-                <Text margin="0.5rem" line='1.25rem'>
-                    Very long and nonsensical text so I can check out if actually long messages and requests from annoying customers will be displayed correctly. It would be sad if they weren't. Normal customers would not care, of course, but this stupid one would. And they are the real problem for the poor hotel staff.
-                </Text>
-                <Text weight="600" width="50%" margin="1rem">Amenitites</Text>
-                <Entry margin="1rem 0 0 0" padding="0" wrap="wrap">
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
-                    </Box>
-                    <Box margin="0" padding="1rem" color="#E8FFEE">
-                        <Text color="#5AD07A">3 Bed Space</Text>
+                    <label id="role-label" htmlFor="role">
+                        <Text weight="600">Role:</Text>
+                        <Select as="select" id="role" color="#135846" weight="600">
+                            <Text as='option' value="manager" color="#135846" weight="400">Manager</Text>
+                            <Text as='option' value="reception" color="#135846" weight="400">Reception</Text>
+                            <Text as='option' value="service" color="#135846" weight="400">Room Service</Text>
+                        </Select>
+                    </label>
+                    <Box as="label" id="description-label" htmlFor="description" margin="0" padding="0" radius="0" height="100%" width="100%">
+                        <Text weight="600">Description:</Text>
+                        <Text as="textarea" id="description" placeholder="Short description..." height="3rem" line="1.25rem" />
                     </Box>
                 </Entry>
+                <Entry margin="1rem 0 0 0" padding="0" radius="0" justify="space-between">
+                    <label id="status-label" htmlFor="status">
+                        <Text weight="600">Status:</Text>
+                        <Select as="select" id="status" color="#135846" weight="600">
+                            <Text as='option' value="active" color="#135846" weight="400">Active</Text>
+                            <Text as='option' value="inactive" color="#135846" weight="400">Inactive</Text>
+                        </Select>
+                    </label>
+                    <Box as="label" id="date-label" htmlFor="date" margin="0" padding="0" radius="0" height="100%" width="100%">
+                        <Text weight="600">Start Date:</Text>
+                        <Text as="input" type="date" id="date" height="3rem" align="center" required />
+                    </Box>
+                </Entry>
+                <label id="password-label" htmlFor="password">
+                    <Text weight="600" margin="1rem">Password:</Text>
+                    <Text as="input" type="text" id="password" placeholder="9assword!" required />
+                </label>
+                <Box as="button" color="#EEF9F2" margin="1rem 0 0 0" padding="1rem 2rem">
+                    <Text weight="600" align="center">Save</Text>
+                </Box>
             </Box>
             <Box width="50%" height="50rem" radius="0" margin="-2rem" padding="0" >
                 <Box as="img" src={empty} width="100%" height="100%" margin="0" padding="0" radius="0" />
