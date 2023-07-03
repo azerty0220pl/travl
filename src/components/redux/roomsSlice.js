162b787ad8377f7c0d4d1c2784e80228ad51a956
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-03 15:51:45 +0200

    redux store and rooms slice created

---
+++ src/components/redux/roomsSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import data from "../../data/rooms.json";
+
+const initialState = {
+    rooms: {},
+    status: "none"
+};
+
+export const fetchRooms = createAsyncThunk('getRooms', () => {
+    return setTimeout(200, () => {
+        return data;
+    })
+});
+
+const roomsSlice = createSlice({
+    name: "rooms",
+    initialState,
+    reducers: {
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchRooms.pending, (state, action) => {
+                state.status = "loading";
+            })
+            .addCase(fetchRooms.fulfilled, (state, action) => {
+                state.status = "success";
+                state.rooms = action.payload;
+            })
+            .addCase(fetchRooms.rejected, (state, action) => {
+                state.status = "error";
+            });
+    }
+});
+
+export default roomsSlice.reducer;
+
