c6c9504d5d7f86fed14ae3da7465fd01ba1b23cb
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-03 19:38:08 +0200

    Typo fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './components/redux/Store';
+import store from './components/redux/store';
 import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
