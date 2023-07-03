44079cc122a08ba940a4530205bc872920eccce3
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-03 15:51:27 +0200

    store provider added to index

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './components/redux/Store';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>
 );
