6ed971ede23d4361f4d56ad6873261b85f2d4740
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-04 09:57:22 +0200

    Minor adjustments

--- src/components/advanced/RoutesComponent.jsx
+++ src/components/advanced/RoutesComponent.jsx
@@ -1,5 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-import { PrivateRoute } from "./PrivateRoute";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "../../pages/Dashboard";
 import Bookings from "../../pages/Bookings";
 import Rooms from "../../pages/Rooms";
@@ -12,19 +11,20 @@ import RoomEdit from "../../pages/RoomEdit";
 const RoutesComponent = ({ modal }) => {
     return (
         <Routes>
-            <Route path="/dashboard" element={<PrivateRoute element={<Dashboard modal={modal} />} />} />
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard modal={modal} />} />
             <Route path="/bookings">
-                <Route index element={<PrivateRoute element={<Bookings modal={modal} />} />} />
-                <Route path="details" element={<PrivateRoute element={<BookingDetails />} />} />
+                <Route index element={<Bookings modal={modal} />} />
+                <Route path="details" element={<BookingDetails />} />
             </Route>
             <Route path="/rooms" >
-                <Route index element={<PrivateRoute element={<Rooms />} />} />
-                <Route path="edit" element={<PrivateRoute element={<RoomEdit />} />} />
+                <Route index element={<Rooms />} />
+                <Route path="edit" element={<RoomEdit />} />
             </Route>
-            <Route path="/contact" element={<PrivateRoute element={<Contact modal={modal} />} />} />
+            <Route path="/contact" element={<Contact modal={modal} />} />
             <Route path="/users" >
-                <Route index element={<PrivateRoute element={<Users modal={modal} />} />} />
-                <Route path="edit" element={<PrivateRoute element={<UserEdit />} />} />
+                <Route index element={<Users modal={modal} />} />
+                <Route path="edit" element={<UserEdit />} />
             </Route>
         </Routes>
     );
