e564b74984bf8fd8b8b6d54e912dee87a41c103d
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-04 09:59:39 +0200

    useRelocation implemented

---
+++ src/components/basic/hooks.js
@@ -0,0 +1,11 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../App";
+
+export const useRelocation = (x) => {
+    const dispatch = useContext(Context).dispatch;
+
+    useEffect(() => {
+        dispatch({ type: "page", page: x });
+    });
+
+}
