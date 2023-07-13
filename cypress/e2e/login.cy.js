fb3fe5641622284b42a1cc5f57374dc9e97c0538
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-13 11:30:35 +0200

    test upgrade

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,40 +4,60 @@ describe('login', () => {
   });
 
   it("user input exists", () => {
-    cy.get('[data-cy="user-input"]');
+    cy.get('[data-cy="user-input"]').should("be.visible");
   });
 
   it("password input exists", () => {
-    cy.get('[data-cy="password-input"]');
+    cy.get('[data-cy="password-input"]').should("be.visible");
   });
 
   it("login button exists", () => {
-    cy.get('[data-cy="login-button"]');
+    cy.get('[data-cy="login-button"]').should("be.visible");
   });
 
   it("no error on empty inputs", () => {
+    cy.get('[data-cy="login-form"]').invoke('submit', (e) => {
+      e.preventDefault();
+      throw new Error('submitted when should not');
+    });
     cy.get('[data-cy="login-button"]').click();
     cy.get('[data-cy="login-error"]').should('not.exist');
+    cy.get('[data-cy="menu"]').should('not.exist');
+    cy.get('[data-cy="login"]').should("be.visible");
   });
 
-  it("cannot login with incorrect credentials", () => {
+  it("no error on empty password", () => {
+    cy.get('[data-cy="login-form"]').invoke('submit', (e) => {
+      e.preventDefault();
+      throw new Error('submitted when should not');
+    });
+    cy.get('[data-cy="user-input"]').type("wrong");
+    cy.get('[data-cy="login-button"]').click();
+    cy.get('[data-cy="login-error"]').should('not.exist');
+    cy.get('[data-cy="menu"]').should('not.exist');
+    cy.get('[data-cy="login"]').should("be.visible");
+  });
+
+  it("error on incorrect credentials", () => {
     cy.get('[data-cy="user-input"]').type("wrong");
     cy.get('[data-cy="password-input"]').type("credentials");
     cy.get('[data-cy="login-button"]').click();
-    cy.get('[data-cy="login-error"]');
+    cy.get('[data-cy="login-error"]').should("be.visible");
+    cy.get('[data-cy="menu"]').should('not.exist');
+    cy.get('[data-cy="login"]').should("be.visible");
   });
 
   it("displays login when not logged", () => {
     cy.visit("localhost:3000/#/bookings");
     cy.get('[data-cy="menu"]').should('not.exist');
-    cy.get('[data-cy="login"]');
+    cy.get('[data-cy="login"]').should("exist");
   });
 
   it("logs in with correct credentials", () => {
     cy.get('[data-cy="user-input"]').type("admin");
     cy.get('[data-cy="password-input"]').type("password");
     cy.get('[data-cy="login-button"]').click();
-    cy.get('[data-cy="menu"]');
+    cy.get('[data-cy="menu"]').should("be.visible");
     cy.get('[data-cy="login"]').should('not.exist');
   });
 });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
         <Entry data-cy="login" $justify="center" $margin="0" $padding="0" $color="transparent" $height="100vh" $width="100%">
             <Box $margin="auto" $width="fit-content">
                 <Text $weight="600">Login:</Text>
-                <Box $margin="1rem 0 0 0" $padding="1rem" $border="1px solid #EBEBEB" as="form" onSubmit={handleSubmit}>
+                <Box data-cy="login-form" $margin="1rem 0 0 0" $padding="1rem" $border="1px solid #EBEBEB" as="form" onSubmit={handleSubmit}>
                     <label id="user-label" htmlFor="user">
                         <Text>Username:</Text>
                         <Text as="input" data-cy="user-input" $margin="0.5rem" type="text" id="user" placeholder="admin" required />
