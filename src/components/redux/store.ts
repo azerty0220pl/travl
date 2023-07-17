2b6e590966e0ed477aefb8492bb25419f8815aa6
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-17 12:32:30 +0200

    Store rewritten in ts

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -3,6 +3,9 @@ import roomsReducer from "./rooms/roomsSlice";
 import bookingsSlice from "./bookingsSlice";
 import usersSlice from "./users/usersSlice";
 import messagesSlice from "./messages/messagesSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux";
 
 const store = configureStore({
     reducer: {
@@ -12,5 +15,11 @@ const store = configureStore({
         messages: messagesSlice
     }
 });
+
+export type Status = "none" | "error" | "success";
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
