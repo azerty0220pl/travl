1d60ecb2f8e02e7462825912e5e392ff4c1d8c06
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-08-11 19:47:29 +0200

    user slice uses API

--- src/components/redux/users/userHooks.ts
+++ src/components/redux/users/userHooks.ts
@@ -1,10 +1,43 @@
-import { User, newUser } from "./usersSlice";
+import { User, changeStatus, newUser } from "./usersSlice";
 import { useAppDispatch } from "../store";
+import { ActionInterface } from "../../../App";
 
 export const useNewUser = () => {
+    const reduxDispatch = useAppDispatch();
     const dispatch = useAppDispatch();
 
-    return (data: Partial<User>) => {
+    return (data: User) => {
         dispatch(newUser(data));
+        reduxDispatch({type: changeStatus, payload: "idle"});
+    };
+}
+
+export const useUpdateUser = () => {
+    const reduxDispatch = useAppDispatch();
+
+    return async (user: User, dispatch: React.Dispatch<ActionInterface>) => {
+        const res = await fetch(
+            process.env.REACT_APP_API +
+            "/users/" + user._id,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token") || ""
+                },
+                body: JSON.stringify({ user: user })
+            }
+        );
+
+        const data = await res.json();
+
+        if (!data.error) {
+            dispatch({
+                type: "user",
+                user: user
+            });
+
+            reduxDispatch({type: changeStatus, payload: "idle"});
+        }
     };
 }
--- src/components/redux/users/usersSlice.ts
+++ src/components/redux/users/usersSlice.ts
@@ -1,60 +1,87 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import data from "../../../data/users.json";
-import { Status } from "../store";
+import { Options, Status } from "../store";
 
 export interface User {
+    _id?: string,
     name: string,
-    id: number,
     email: string,
     joined: string,
     description: string,
     phone: string,
-    status: "Inactive" | "Active",
+    role: number,
+    status: boolean,
     password: string
 };
 
 export interface UserState {
-    users: Partial<User>[],
-    status: Status
+    status: Status,
+    users: User[],
+    count: number
 };
 
-const initialState : UserState = {
+const initialState: UserState = {
+    status: "idle",
     users: [],
-    status: "none"
+    count: 0
 };
 
-export const fetchUsers = createAsyncThunk('getUsers', () => {
-    return new Promise<Partial<User>[]>((resolve) => {
-        setTimeout(() => {
-            resolve(data as Partial<User>[]);
-        }, 200);
-    });
+export const fetchUsers = createAsyncThunk('getUsers', async (options: Options) => {
+    const res = await fetch(
+        process.env.REACT_APP_API +
+        "/users?page=" + options.page +
+        "&limit=" + options.limit +
+        "&filter=" + options.filter +
+        "&order=" + options.order,
+        {
+            method: "GET",
+            headers: {
+                Authorization: localStorage.getItem("token") || ""
+            }
+        });
+
+    return res.json();
 });
 
-export const newUser = createAsyncThunk('newUser', (user: Partial<User>) => {
-    return new Promise<Partial<User>>((resolve) => {
-        setTimeout(() => {
-            resolve(user);
-        }, 200);
-    });
+export const newUser = createAsyncThunk('newUser', async (user: User) => {
+    const res = await fetch(
+        process.env.REACT_APP_API + "/users",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: localStorage.getItem("token") || ""
+            },
+            body: JSON.stringify({ user: user })
+        });
+
+    return res.json();
 });
 
 const usersSlice = createSlice({
     name: "users",
     initialState,
     reducers: {
+        changeStatus: (state, action) => {
+            state.status = action.payload;
+        }
     },
     extraReducers: builder => {
         builder
             .addCase(fetchUsers.fulfilled, (state, action) => {
-                state.status = "success";
-                state.users = action.payload;
+                state.status = "fulfilled";
+                state.users = action.payload.user;
+                state.count = action.payload.count;
             })
-            .addCase(newUser.fulfilled, (state, action) => {
-                state.users.push(action.payload);
-            });
+            .addCase(fetchUsers.pending, (state) => {
+                state.status = "pending";
+            })
+            .addCase(fetchUsers.rejected, (state) => {
+                state.status = "rejected";
+                state.users = [];
+                state.count = 0;
+            })
     }
 });
 
+export const { changeStatus } = usersSlice.actions;
 export default usersSlice.reducer;
-
