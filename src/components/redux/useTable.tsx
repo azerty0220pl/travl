e43a5a821a68f09cc22c2f31a9a0227e6c3bdc11
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-08-12 15:11:06 +0200

    Adapted to all data

--- src/components/redux/useTable.tsx
+++ src/components/redux/useTable.tsx
@@ -10,79 +10,6 @@ import { Room } from "./rooms/roomsSlice";
 import { Message } from "./messages/messagesSlice";
 import MessageElement from "../advanced/Message";
 
-interface Orders { [index: string]: (a: any, b: any) => number };
-const orders: Orders = {
-    order: (a: Book, b: Book) => {
-        //return Date.parse(a.order) < Date.parse(b.order) ? -1 : 1;
-        return 1;
-    },
-    name: (a: Room | Book | User | Message, b: Room | Book | User | Message) => {
-        return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
-    },
-    in: (a: Book, b: Book) => {
-        //return Date.parse(a.in) < Date.parse(b.in) ? -1 : 1;
-        return 1;
-    },
-    out: (a: Book, b: Book) => {
-        //return Date.parse(a.out) < Date.parse(b.out) ? -1 : 1;
-        return 1;
-    },
-    number: (a: Room | Book | User | Message, b: Room | Book | User | Message) => {
-        return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
-    },
-    ascending: (a: Room, b: Room) => {
-        if (a.price === b.price)
-            return 0;
-        return a.price < b.price ? -1 : 1;
-    },
-    descending: (a: Room, b: Room) => {
-        if (a.price === b.price)
-            return 0;
-        return a.price < b.price ? 1 : -1;
-    },
-    status: (a: Room, b: Room) => {
-        if (a.status === b.status)
-            return 0;
-        return a.status === "Available" ? -1 : 1;
-    },
-    joined: (a: User, b: User) => {
-        return Date.parse(a.joined) < Date.parse(b.joined) ? -1 : 1;
-    },
-    date: (a: Message, b: Message) => {
-        return Date.parse(a.date) < Date.parse(b.date) ? -1 : 1;
-    },
-};
-
-interface Filters { [index: string]: (x: any) => boolean };
-const filters: Filters = {
-    none: () => true,
-    progress: (x: Book) => {
-        //return x.status === "In Progress";
-        return true;
-    },
-    available: (x: Room) => {
-        return x.status === "Available";
-    },
-    booked: (x: Room) => {
-        return x.status === "Booked";
-    },
-    active: (x: User) => {
-        return x.status === true //"Active";
-    },
-    inactive: (x: User) => {
-        return x.status === false //"Inactive";
-    },
-    published: (x: Message) => {
-        return !x.archived;
-    },
-    archived: (x: Message) => {
-        return x.archived;
-    },
-    read: (x: Message) => {
-        return !x.read && !x.archived;
-    }
-}
-
 interface Selector {
     [index: string]: {
         data: (state: RootState) => any,
@@ -91,9 +18,21 @@ interface Selector {
 };
 
 const selectors: Selector = {
+    messages: {
+        data: (state: RootState) => state.messages.messages,
+        count: (state: RootState) => state.messages.count
+    },
+    rooms: {
+        data: (state: RootState) => state.rooms.rooms,
+        count: (state: RootState) => state.rooms.count
+    },
     users: {
         data: (state: RootState) => state.users.users,
         count: (state: RootState) => state.users.count
+    },
+    bookings: {
+        data: (state: RootState) => state.bookings.bookings,
+        count: (state: RootState) => state.bookings.count
     }
 }
 
