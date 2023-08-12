469148dda2af33b64f84c25ac99495bb4358cfff
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-08-12 15:11:25 +0200

    userHooks working

--- src/components/redux/users/userHooks.ts
+++ src/components/redux/users/userHooks.ts
@@ -2,42 +2,42 @@ import { User, changeStatus, newUser } from "./usersSlice";
 import { useAppDispatch } from "../store";
 import { ActionInterface } from "../../../App";
 
-export const useNewUser = () => {
-    const reduxDispatch = useAppDispatch();
-    const dispatch = useAppDispatch();
+const reduxDispatch = useAppDispatch();
 
+export const useNewUser = () => {
     return (data: User) => {
-        dispatch(newUser(data));
-        reduxDispatch({type: changeStatus, payload: "idle"});
+        reduxDispatch(newUser(data));
+        reduxDispatch({ type: changeStatus, payload: "idle" });
     };
 }
 
 export const useUpdateUser = () => {
-    const reduxDispatch = useAppDispatch();
-
     return async (user: User, dispatch: React.Dispatch<ActionInterface>) => {
-        const res = await fetch(
-            process.env.REACT_APP_API +
-            "/users/" + user._id,
-            {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: localStorage.getItem("token") || ""
-                },
-                body: JSON.stringify({ user: user })
+        let res: Response;
+        try {
+            res = await fetch(
+                process.env.REACT_APP_API +
+                "/users/" + user._id,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: localStorage.getItem("token") || ""
+                    },
+                    body: JSON.stringify({ user: user })
+                }
+            );
+
+            const data = await res.json();
+
+            if (!data.error) {
+                dispatch({
+                    type: "user",
+                    user: user
+                });
+
+                reduxDispatch({ type: changeStatus, payload: "idle" });
             }
-        );
-
-        const data = await res.json();
-
-        if (!data.error) {
-            dispatch({
-                type: "user",
-                user: user
-            });
-
-            reduxDispatch({type: changeStatus, payload: "idle"});
-        }
+        } catch { }
     };
 }
