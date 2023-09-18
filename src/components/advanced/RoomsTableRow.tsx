119543d8ae51a396671f0d024ae77d332986f111
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-09-18 16:20:03 +0200

    Rooms row little presentation upgrade

--- src/components/advanced/RoomsTableRow.tsx
+++ src/components/advanced/RoomsTableRow.tsx
@@ -21,43 +21,59 @@ const RoomsTableRow = ({ x, i }: { x: Room, i: number }) => {
             );
     }
 
-return (
-    <Row key={i}>
-        <Cell>
-            <Box $display="inline" as="img" $padding="0" $margin="0" $width="5rem" $height="3rem" src={empty} />
-        </Cell>
-        <Cell>
-            <Text data-cy={"room-" + x._id + "name"}>{x.name}</Text>
-        </Cell>
-        <Cell>
-            <Text>{x._id!.slice(-6)}</Text>
-        </Cell>
-        <Cell>
-            <Text>{x.type}</Text>
-        </Cell>
-        <Cell>
-            <Box $margin="0" $padding="0" $radius="0">
-                <Text $line="1.25rem">{x.ammenities}</Text>
-            </Box>
-        </Cell>
-        <Cell>
-            <Entry $gap="0" $padding="0 1rem 0 0" $radius="0">
-                <Text $width="fit-content">{x.price}</Text>
-                <Text $size="0.75rem" $color="#799283">/Night</Text>
-            </Entry>
-        </Cell>
-        <Cell>
-            <Entry $gap="0" $padding="0 1rem 0 0" $radius="0">
-                <Text $width="fit-content">{x.price! * (100 - x.offer!) / 100}</Text>
-                <Text $size="0.75rem" $color="#799283">/Night</Text>
-            </Entry>
-        </Cell>
-        <Cell>
-            {
-            }
-        </Cell>
-    </Row>
-);
+    const types = (type) => {
+        switch (type) {
+            case 0:
+                return "Single Bed";
+            case 1:
+                return "Double Bed";
+            case 2:
+                return "Double Superior";
+            case 3:
+                return "Suite";
+            default:
+                return "";
+        }
+    }
+
+    return (
+        <Row key={i}>
+            <Cell>
+                <Box $display="inline" as="img" $padding="0" $margin="0" $width="5rem" $height="3rem" src={empty} />
+            </Cell>
+            <Cell>
+                <Text data-cy={"room-" + x._id + "name"}>{x.name}</Text>
+            </Cell>
+            <Cell>
+                <Text>{x._id!.slice(-6)}</Text>
+            </Cell>
+            <Cell>
+                <Text>{types(x.type)}</Text>
+            </Cell>
+            <Cell>
+                <Box $margin="0" $padding="0" $radius="0">
+                    <Text $line="1.25rem">{x.ammenities}</Text>
+                </Box>
+            </Cell>
+            <Cell>
+                <Entry $gap="0" $padding="0 1rem 0 0" $radius="0">
+                    <Text $width="fit-content">{x.price}</Text>
+                    <Text $size="0.75rem" $color="#799283">/Night</Text>
+                </Entry>
+            </Cell>
+            <Cell>
+                <Entry $gap="0" $padding="0 1rem 0 0" $radius="0">
+                    <Text $width="fit-content">{x.price! * (100 - x.offer!) / 100}</Text>
+                    <Text $size="0.75rem" $color="#799283">/Night</Text>
+                </Entry>
+            </Cell>
+            <Cell>
+                {
+                    status(true)
+                }
+            </Cell>
+        </Row>
+    );
 }
 
 export default RoomsTableRow;
